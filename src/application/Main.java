c46747d7ea4f22fcc92d24f022cdaec3b17b23b1
kimcassy31 <kim.cassy@freenet.de>
2017-06-21 13:07:40 +0200

    Programm

---
+++ src/application/Main.java
@@ -0,0 +1,26 @@
+package application;
+
+
+
+/**
+ * Die Klassen von MemoryField, wurden getetest und funktionieren.
+ * Die Mechanik ist mit der GUI von Najbir eingebunden.
+ * Aus diesen Grund ist die Main-Methode hier nicht relevant.
+ */
+
+
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+	}
+	
+	public static void main(String[] args) {
+	}
+}
